790ac35e3fcad877c5595310b292c66bafe1c7d2
Harness SAST Auto-Remediation <sast-remediation@harness.io>
2026-07-28 19:04:54 +0000

    vuln-fix: Semgrep Finding: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage (STO-iZDJDlVO9rSgpTV9t1G85g, CWE-352) | Low | 1 location

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-// const csrf = require('csurf');
+const csrf = require('csurf');
 const consolidate = require("consolidate"); // Templating library adapter for Express
 const swig = require("swig");
 // const helmet = require("helmet");
@@ -85,16 +85,11 @@ MongoClient.connect(db, (err, db) => {
         }
     }));
 
-    /*
-    // Fix for A8 - CSRF
-    // Enable Express csrf protection
     app.use(csrf());
-    // Make csrf token available in templates
     app.use((req, res, next) => {
         res.locals.csrftoken = req.csrfToken();
         next();
     });
-    */
 
     // Register templating engine
     app.engine(".html", consolidate.swig);
